26a71855216c80437869ed16e7b099c41de893d8
Leandro Reis <leandro.reiz@gmail.com>
2019-12-19 02:07:42 -0300

    added conditionals

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ console.log(typeof isOlder);
 */
 /************************************* 
  * Operator Precedence
-*/
+
 var now = 2019;
 var yearLeandro = 1983;
 var fullAge = 18;
@@ -72,21 +72,56 @@ var fullAge = 18;
 var isFullAge = now - yearLeandro >= fullAge;
 console.log(isFullAge);
 
+/***************************************
+ * CODING CHALLENGE #1
+ */
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula:
+BMI = mass / height^2 = mass / (height * height).
+(mass in kg and height in meter).
+
+1. Store Mark's and John's mass in variables;
+2. Calculate both their BMIs;
+3. Create a boolean variable containing information whether Mark has a higher BMI than John; 
+4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true").
 
 
+var markBMI, johnBMI, mass, height;
+var isMarkBMIHigher;
 
+mass = prompt("Insert Mark's mass: ");
+height = prompt("Insert Mark's height: ");
+markBMI = mass / (height*height);
 
+mass = prompt("Insert John's mass: ");
+height = prompt("Insert John's height: ");
+johnBMI = mass / (height*height);
 
+isMarkBMIHigher = markBMI > johnBMI;
+console.log(markBMI, johnBMI);
+console.log("Is Mark's BMI higher than John's?" + isMarkBMIHigher);
 
+var firstName = 'John';
+var civilStatus = 'single';
 
+if (civilStatus === 'married') {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon!');
+}
+*/
+var markBMI, johnBMI, mass, height;
 
+mass = prompt("Insert Mark's mass: ");
+height = prompt("Insert Mark's height: ");
+markBMI = mass / (height*height);
 
+mass = prompt("Insert John's mass: ");
+height = prompt("Insert John's height: ");
+johnBMI = mass / (height*height);
 
-
-
-
-
-
-
-
-
+if (markBMI > johnBMI) {
+    console.log('Mark\'s BMI is higher than John\'s.');
+} else {
+    console.log('John\'s BMI is higher than Mark\'s?');
+}
