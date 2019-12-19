e245a0d0bc164dc404c3f8059aeb0dff130e31a4
Leandro Reis <leandro.reiz@gmail.com>
2019-12-18 23:26:30 -0300

    added type coercion and variable mutation

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Javascript Basic Commands
 Date: 17/12/19
 
-* Print a message on console: console.log('<message>');
-* Declare a variable: var <variable-name> = <value>;
-    In Javascript you don't need to declare the type of a variable. In this case the type will be automatically defined as 'undefined'.
-* // line comment
-* /**/ multi-line/block comment
+* console.log('message'); -> Print a message on console
+* var variable-name = value; -> Declare a variable
+    In Javascript you don't need to declare the type of a variable. In this case the type will be automatically defined as 'undefined'
+* // -> line comment
+* /**/ -> multi-line/block comment
+* alert() -> Creates a pop-up window with the message contained between the parenthesis
+* prompt() -> Asks for an input from the user that can be stored in a variable
 
 # Primitive Javascript Data Types
 
@@ -19,3 +21,9 @@ Logical data type that can only be true or false
 Data Type of a variable that does not have a value yet
 ### null
 Also means 'non-existent'
+
+### Type coercion
+Is the process of converting value from one type to another (such as string to number, object to boolean, and so on).
+
+### Variable mutation
+When you mutate a variable you are modifying the original value.
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//declaring variables and printing them on console
+/*
 var firstName = 'Leandro';
 var lastName = 'Reis';
 var age = 28;
@@ -8,3 +10,83 @@ console.log('Nome: ' + firstName +' '+lastName);
 console.log('Idade: ' + age);
 console.log(fullAge);
 console.log(job);
+
+//type coercion
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+
+//Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+
+lastName = prompt("What is " +firstName+ "'s last name?");
+console.log(firstName + ' ' + lastName);
+*/
+/********************************* 
+ * Basic Operatiors
+
+var now, yearJohn, yearLeandro; 
+now = 2019;
+ageJohn = 28;
+ageLeandro = 36;
+
+//Math Operators
+yearJohn = now - ageJohn;
+yearLeandro = now - ageLeandro;
+
+console.log(yearLeandro);
+
+console.log(now+20);
+console.log(now*2);
+console.log(now/10);
+
+// Logical Operators
+var isOlder;
+isOlder = ageJohn < ageLeandro; // check two or more values and returns a boolean with the result
+console.log(isOlder);
+
+// typeof Operator
+var x;
+console.log(typeof x);
+console.log(typeof 'Leandro is older than John');
+console.log(typeof ageLeandro);
+console.log(typeof isOlder);
+*/
+/************************************* 
+ * Operator Precedence
+*/
+var now = 2019;
+var yearLeandro = 1983;
+var fullAge = 18;
+
+var isFullAge = now - yearLeandro >= fullAge;
+console.log(isFullAge);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
